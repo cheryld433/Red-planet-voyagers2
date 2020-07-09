97629fd77612e0485f1aa3bcae7aff366d4a97d3
Cheryl Daniels <cheryld433@gmail.com>
2020-07-08 23:20:53 -0400

    added .gtiignore, modified server.js for mongoose connect

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.set("view engine", "handlebars");
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
